76f70d296e0bdb3a4efc3d74ad8cb834fc97a707
draelios <acantefo7@gmail.com>
2023-02-26 19:03:53 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/lib.rs
    	modified:   src/main.rs
    
    Changed code to use iterators to make it more idiomatic

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,37 @@
 use std::{fs, env};
 use std::error::Error;
 
-pub struct Search<'a> {
-    pub query: &'a str,
-    pub file_path: &'a str,
+pub struct Search {
+    pub query: String,
+    pub file_path: String,
     pub ignore_case: bool,
 }
 
-impl<'a> Search<'a> {
-    fn new(query: &'a str, file_path: &'a str, ignore_case: bool) -> Self{
+impl Search {
+    fn new(query: String, file_path: String, ignore_case: bool) -> Self{
         Self { query, file_path, ignore_case }
     }
 
-    pub fn build(args: &'a [String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!")
-        }
-
-        let ignore_case = 
-            if args.len()==4 && &args[3] == "true"{true} 
-            else {env::var("IGNORE_CASE").is_ok()};
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
         
-        Ok(Search::new(&args[1], &args[2], ignore_case))
+        args.next();  // we remove the prgram directory 
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = match args.next().as_deref() {
+            Some("true") => true,
+            None => env::var("IGNORE_CASE").is_ok(),
+            _ => env::var("IGNORE_CASE").is_ok(),
+        };
+        
+        Ok(Search::new(query, file_path, ignore_case))
     }
 }
 
@@ -29,9 +39,9 @@ pub fn run (search: Search) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(search.file_path)?;
 
     let results = if search.ignore_case {
-        search_words_case_insensitive(&content, search.query)
+        search_words_case_insensitive(&content, &search.query)
     } else{
-        search_words(&content, search.query)
+        search_words(&content, &search.query)
     };
 
     for line in results {
@@ -40,37 +50,23 @@ pub fn run (search: Search) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn search_words<'a>(content: &'a str, query: &str) -> Vec<&'a str> {
-    
-    let mut finds: Vec<&str> = vec![];
-    
-    for line in content.lines() {
-        if line.contains(query) {
-            finds.push(line);
-        }
-    }
-
-    finds
+fn search_words<'a>(content: &'a str, query: &str) -> Vec<&'a str> {  
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 } 
 
 
 fn search_words_case_insensitive<'a>(content: &'a str, query: &str) -> Vec<&'a str> {
     
-    let mut finds: Vec<&str> = vec![];
-    let query = query.to_lowercase();
-    
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            finds.push(line);
-        }
-    }
-
-    finds
+    content
+        .lines()
+        .filter(|line| {
+            line.to_lowercase().contains(&query.to_lowercase())
+        })
+        .collect()
 } 
-
-
-
-
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::env;
 use grep_clone::Search;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let search = Search::build(&args).unwrap_or_else(|err| {
+    let search = Search::build(env::args()).unwrap_or_else(|err| {
         eprintln!("{err}");
         process::exit(1);
     });
